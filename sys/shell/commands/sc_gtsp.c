c48c2160cb110d29703b3430e32be9f6110c4601
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2014-06-08 16:31:17 +0200

    shell commands for gtsp

---
+++ sys/shell/commands/sc_gtsp.c
@@ -0,0 +1,43 @@
+/**
+ * Shell commands for the gtsp clock-sync module
+ *
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ *
+ * @ingroup shell_commands
+ * @{
+ * @file    sc_gtsp.c
+ * @brief   Provides interactive access to the GTSP clock-sync module.
+ * @author  Philipp Rosenkranz <philipp.rosenkranz@fu-berlin.de>
+ * @}
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+#include "gtsp.h"
+
+static void _print_help(void);
+
+void _gtsp(int argc, char **argv) {
+	if (argc == 2) {
+		if (!strcmp(argv[1], "on")) {
+			gtsp_resume();
+			return;
+		}
+		if (!strcmp(argv[1], "off")) {
+			gtsp_pause();
+			return;
+		}
+	}
+	_print_help();
+}
+
+static void _print_help(void) {
+	printf("Usage: gtsp on|off \n");
+}
