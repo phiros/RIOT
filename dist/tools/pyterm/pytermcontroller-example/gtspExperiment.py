ac1e562bd8a5aecff3e01a1e996e430ebaa7a3b2
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2014-07-06 12:06:46 +0200

    changed config parameters in example

--- dist/tools/pyterm/pytermcontroller-example/gtspExperiment.py
+++ dist/tools/pyterm/pytermcontroller-example/gtspExperiment.py
@@ -8,25 +8,25 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../testbeds"))
 from pytermcontroller import Experiment, ExperimentRunner
 from testbeds import DESTestbed
 
-serverHost = "uhu"
-serverPort = 1025
-userName = "phiros"
-basePath = "/home/phiros"
-flasher = basePath + "/testbed/bin/n2n_flash.py"
+serverHost =  "uhu"
+serverPort =  1025
+userName =    "phiros"
+basePath =    "/home/phiros"
+flasher =     basePath + "/testbed/bin/n2n_flash.py"
 hexFilePath = basePath + "/testbed/toflash.hex"
-pyterm = basePath + "/testbed/RIOT/dist/tools/pyterm/pyterm.py" 
+pyterm =      basePath + "/testbed/RIOT/dist/tools/pyterm/pyterm.py" 
 logFilePath = basePath + "/testbed/.pyterm/log"
-hostFile = basePath + "/testbed/hosts"
+hostFile =    basePath + "/testbed/hosts"
 
 class GTSPExperiment(Experiment): 
     def preHook(self):
         self.readHostFile(hostFile)
       
     def start(self):
-        self.waitAndCall(5, self.setup)
-        self.waitAndCall(1, self.enableGTSP)
-        self.waitAndCall(1, self.disableGTSP)
-        self.waitAndCall(1, self.stop)     
+        self.waitAndCall(1*60,   self.setup)
+        self.waitAndCall(15*60 , self.enableGTSP)
+        self.waitAndCall(15*60 , self.disableGTSP)
+        self.waitAndCall(15*60 , self.stop)     
         
     def setup(self):
         for host, connection in self.clientIterator():
@@ -49,5 +49,6 @@ class GTSPExperiment(Experiment):
                
 testbed = DESTestbed(serverHost, serverPort, userName, flasher, 
                      hexFilePath, pyterm, logFilePath, hostFile)
+testbed.flashNodes()
 experiment = ExperimentRunner(GTSPExperiment, testbed) 
 experiment.run()
