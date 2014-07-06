537e9a036060518d268b4cc4f9b8645ede28f19c
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2014-07-06 12:37:54 +0200

    pyterm execution fixed

--- dist/tools/pyterm/pytermcontroller-example/gtspExperiment.py
+++ dist/tools/pyterm/pytermcontroller-example/gtspExperiment.py
@@ -14,7 +14,7 @@ userName =    "phiros"
 basePath =    "/home/phiros"
 flasher =     basePath + "/bin/n2n_flash.py"
 hexFilePath = basePath + "/toflash.hex"
-pyterm =      basePath + "/RIOT/dist/tools/pyterm/pyterm.py" 
+pyterm =      basePath + "/RIOT/dist/tools/pyterm/pyterm.py -s " + serverHost + "-P " + str(serverPort) 
 logFilePath = basePath + "/testbed/.pyterm/log"
 hostFile =    basePath + "/testbed/hosts"
 
