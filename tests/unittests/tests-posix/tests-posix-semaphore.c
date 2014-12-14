7d3bad9a9e653f3ebc6820508d6cb2f020720f2e
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2014-12-14 23:22:08 +0100

    [SQUASHME] threaded code doesn't work yet

--- tests/unittests/tests-posix/tests-posix-semaphore.c
+++ tests/unittests/tests-posix/tests-posix-semaphore.c
@@ -17,37 +17,63 @@ char simple_thread_stack[KERNEL_CONF_STACKSIZE_MAIN];
 static sem_t semaphore;
 static kernel_pid_t simple_pid;
 
-static mutex_t assertion_mutex;
-#define TEST_ASSERT_THREADED_EQUAL_INT(expected_, actual_) \
+#define TEST_MSG_FAIL (1)
+#define TEST_MSG_SUCCESS (0)
+
+static int thread_counter = 0;
+
+#define TEST_FAIL_THREADED(coordinator_pid) \
     do { \
-        long long ____expected__ = (long long) (expected_); \
-        long long ____actual__ = (long long) (actual_); \
-        if (____expected__ != ____actual__) { \
-            mutex_lock(&assertion_mutex); \
-            assertImplementationLongLong(____expected__, ____actual__, __LINE__, __FILE__); \
-            mutex_unlock(&assertion_mutex); \
-        } \
+        msg_t m; \
+        m.content.value = TEST_MSG_FAIL; \
+        msg_send(&m, coordinator_pid); \
     } while (0)
+
+#define TEST_SUCCESS_THREADED(coordinator_pid) \
+    do { \
+        msg_t m; \
+        m.content.value = TEST_MSG_SUCCESS; \
+        msg_send(&m, coordinator_pid); \
+    } while (0)
+
+#define RUN_AS_UNITTEST_THREAD(thread, arg, stack, prio) \
+    do { \
+        thread_counter++; \
+        thread_create(stack, sizeof(stack), \
+                      prio, CREATE_STACKTEST, thread, arg, ""); \
+    } while (0)
+
+
+
+static void test_coordinator(void)
+{
+    msg_t m;
+    while(1) {
+        if(msg_try_receive(&m) == 1)
+        {
+            if(!m.content.value) TEST_ASSERT(0); // leads to threads starving
+            else thread_counter--;
+        }
+        if(thread_counter <= 0) TEST_ASSERT(1);
+    }
+}
 
 static void *simple_thread(void *arg)
 {
-    (void) arg;
-    TEST_ASSERT_THREADED_EQUAL_INT(0, 1);
+    TEST_FAIL_THREADED(*((kernel_pid_t *) arg));
     return NULL;
 }
 
-static void tests_posix_semaphore_two_threads(void)
+static void test_simple_thread(void)
 {
-    mutex_init(&assertion_mutex);
-    simple_pid = thread_create(simple_thread_stack,
-                                         sizeof(simple_thread_stack),
-                                         PRIORITY_MAIN - 1,
-                                         CREATE_STACKTEST | CREATE_WOUT_YIELD,
-                                         simple_thread,
-                                         NULL,
-                                         "second");
-    TEST_ASSERT_THREADED_EQUAL_INT(0, (int) simple_pid);
+    kernel_pid_t main_pid = thread_getpid();
+    // start threads
+    RUN_AS_UNITTEST_THREAD(simple_thread, &main_pid, simple_thread_stack, PRIORITY_MAIN -1);
+    // start coordinator
+    test_coordinator();
 }
+
+
 
 static void tests_posix_semaphore_two_threads_with_priority(void)
 {
@@ -57,8 +83,8 @@ static void tests_posix_semaphore_two_threads_with_priority(void)
 Test *tests_posix_semaphore_tests(void)
 {
     EMB_UNIT_TESTFIXTURES(fixtures) {
-        new_TestFixture(tests_posix_semaphore_two_threads),
-        new_TestFixture(tests_posix_semaphore_two_threads_with_priority),
+    new_TestFixture(test_simple_thread),
+    new_TestFixture(tests_posix_semaphore_two_threads_with_priority),
 };
 
 EMB_UNIT_TESTCALLER(posix_tests, NULL, NULL, fixtures);
